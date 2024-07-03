f2eaff73534a926d657c0c73a58cce10389bc19c
Cláudio Dias <113221142+soyclaudiodias@users.noreply.github.com>
2024-07-03 10:22:53 -0300

    Create 1020 - Idade em Dias.c

---
+++ Iniciante/1020 - Idade em Dias.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+
+  scanf("%d", &n);
+  printf("%d ano(s)\n", n/365);
+  printf("%d mes(es)\n", (n-365*(n/365))/30);
+  printf("%d dia(s)\n", (n-365*(n/365))%30);
+  
+  return 0;
+}

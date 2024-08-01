55fc045f6d12d3f44ddc4e02b072d144bed03e6f
Cláudio Dias <113221142+soyclaudiodias@users.noreply.github.com>
2024-08-01 10:55:15 -0300

    Create 1116 - Dividindo X por Y.c

---
+++ Iniciante/1116 - Dividindo X por Y.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+    int n;
+    float x, y;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%f %f", &x, &y);
+
+        if (y == 0)
+            printf("divisao impossivel\n");
+        else 
+            printf("%.1f\n", (float) x / y);
+    }
+    
+    return 0;
+}

26f52f54b5ccf810df4fa5e400fef92770f1913e
Cláudio Dias <113221142+soyclaudiodias@users.noreply.github.com>
2025-01-06 10:33:03 -0300

    Create 1144 - Sequência Lógica.c

---
+++ Iniciante/1144 - Sequência Lógica.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        printf("%.f %.f %.f\n", pow(i, 1), pow(i, 2), pow(i, 3));
+        printf("%.f %.f %.f\n", pow(i, 1), (pow(i, 2) + 1), (pow(i, 3) + 1));
+    }
+ 
+    return 0;
+}

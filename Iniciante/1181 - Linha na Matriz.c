75a2b6f018d28b04b311075e740eaedb5c00d73c
Cláudio Dias <113221142+soyclaudiodias@users.noreply.github.com>
2025-04-21 11:39:16 -0300

    Create 1181 - Linha na Matriz.c

---
+++ Iniciante/1181 - Linha na Matriz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+    float m[12][12];
+    int l, sum = 0;
+    char t[2];
+
+    scanf("%d", &l);
+    scanf("%s", t);
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++)
+            scanf("%f", &m[i][j]);
+    }
+
+    for (int i = 0; i < 12; i++)
+        sum += m[l][i];
+
+    if ((strcmp(t, "S")) == 0)
+        printf("%.1f\n", (float) sum);
+    else if ((strcmp(t, "M")) == 0)
+        printf("%.1f\n", (float) sum / 12);
+
+    return 0;
+}

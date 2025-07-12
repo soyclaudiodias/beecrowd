83f3f2e628d8e85a993400ec1c08d95c3ed5f7cb
Cláudio Dias <113221142+soyclaudiodias@users.noreply.github.com>
2025-07-11 23:05:25 -0300

    Create 1190 - Área Direita.c

---
+++ Iniciante/1190 - Área Direita.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    double m[12][12], sum = 0;
+    int dec_j = 11;
+    char o[2];
+
+    scanf("%s", o);
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++)
+            scanf("%lf", &m[i][j]);
+    }
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            if (i < j && j > dec_j)
+                sum += m[i][j];
+        }
+
+        dec_j--;
+    }
+
+    if ((strcmp(o, "S")) == 0)
+        printf("%.1lf\n", (double)sum);
+    else if ((strcmp(o, "M")) == 0)
+        printf("%.1lf\n", (double)sum / 30);
+
+    return 0;
+}

61bc4ae3a79c7534f6ebea987f14ae8102092f2d
Cláudio Dias <113221142+soyclaudiodias@users.noreply.github.com>
2025-07-26 18:45:55 -0300

    Create 1589 - Bob Conduite.c

---
+++ Iniciante/1589 - Bob Conduite.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int t, r1, r2;
+
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        scanf("%d %d", &r1, &r2);
+
+        printf("%d\n", r1+r2);
+    }
+
+    return 0;
+}

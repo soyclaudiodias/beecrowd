c18ec577aea1e8bfad052ea0757a1b30d01e4f42
Cláudio Dias <113221142+soyclaudiodias@users.noreply.github.com>
2025-08-12 15:54:29 -0300

    Create 1924 - Vitória e Sua Indecisão.c

---
+++ Iniciante/1924 - Vitória e Sua Indecisão.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    char s[100];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        scanf("%s", s);
+
+    printf("Ciencia da Computacao\n");
+
+    return 0;
+}
